fd98acf22f08f15324adc490f38bca41c949edb5
danbirch9000 <danny_birch@hotmail.com>
2016-07-25 20:39:52 +0100

    added cors support

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var mongoose    = require('mongoose');
 var passport	= require('passport');
 var config      = require('./config/database'); // get db config file
 var User        = require('./modules/user/server/models/user.server.models'); // get the mongoose model
-
+var cors = require('cors');
 var port        = process.env.PORT || 8080;
 var jwt         = require('jwt-simple');
 var user_routes      = require('./modules/user/server/routes/user.server.routes');
@@ -24,6 +24,9 @@ app.use(morgan('dev'));
 // Use the passport package in our application
 app.use(passport.initialize());
  
+
+ app.use(cors());
+ 
 // demo Route (GET http://localhost:8080)
 app.get('/', function(req, res) {
   res.send('Hello! The API is at http://localhost:' + port + '/api');
@@ -34,6 +37,7 @@ mongoose.connect(config.database);
  
 // pass passport for configuration
 require('./config/passport')(passport);
+ 
  
 // bundle our routes
 var apiRoutes = express.Router();
@@ -74,6 +78,8 @@ getToken = function (headers) {
   }
 };
  
+
+
 // connect the api routes under /api/*
 app.use('/api', apiRoutes);
 
